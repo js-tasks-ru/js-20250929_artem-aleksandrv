cae045532c463f6bca3e421aaaf3f19325b9d607
ARTALEAL <artaleal@yandex.ru>
2025-09-29 21:24:31 +0300

    2-pick task

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,5 +5,14 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
-
+  const entries = Object.entries(obj);
+  let res = {};
+  entries.forEach((val) => {
+    if (fields.includes(val[0])) {
+      let key = val[0];
+      let value = val[1];
+      res = { ...res, [key]: value };
+    }
+  });
+  return res;
 };
