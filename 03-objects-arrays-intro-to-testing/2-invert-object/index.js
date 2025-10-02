ba9d87953ea9d2c097811d499520d67965396b95
ARTALEAL <artaleal@yandex.ru>
2025-10-02 21:28:28 +0300

    инвертировал проверку в invert object

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,13 +4,13 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-  if (obj) {
-    const entries = Object.entries(obj);
-    const result = [];
-    for (let entry of entries) {
-      result.push(entry.reverse());
-    }
-    return Object.fromEntries(result);
+  if (!obj) {
+    return;
   }
-  return undefined;
+  const entries = Object.entries(obj);
+  const result = [];
+  for (let entry of entries) {
+    result.push(entry.reverse());
+  }
+  return Object.fromEntries(result);
 }
