0dd38df4aa09cf10a25ba017118419d925fd530c
ARTALEAL <artaleal@yandex.ru>
2025-11-07 22:10:55 +0300

    delete old commented code

--- 06-events-practice/3-double-slider/index.js
+++ 06-events-practice/3-double-slider/index.js
@@ -37,139 +37,85 @@ export default class DoubleSlider {
   }
 
   processPointerMove = (event) => {
-    const sliderContainer = document.querySelector('.range-slider__inner');
-    const {left, width} = sliderContainer.getBoundingClientRect();
+    const sliderContainer = document.querySelector(".range-slider__inner");
+    const { left, width } = sliderContainer.getBoundingClientRect();
 
     const containerLeftX = left;
     const containerRightX = left + width;
     const pointerX = event.clientX;
-    const normalizedPointerX = Math.min(containerRightX, Math.max(containerLeftX, pointerX));
-    const percentPointerX = Math.round((normalizedPointerX - containerLeftX) / (containerRightX - containerLeftX) * 100);
-    return this.min + (this.max - this.min) * percentPointerX / 100;
+    const normalizedPointerX = Math.min(
+      containerRightX,
+      Math.max(containerLeftX, pointerX)
+    );
+    const percentPointerX = Math.round(((normalizedPointerX - containerLeftX) / (containerRightX - containerLeftX)) * 100
+    );
+    return this.min + ((this.max - this.min) * percentPointerX) / 100;
+  };
+
+  onThumbPointerDown = (event) => {
+    this.activeThumb = event.target.dataset.element;
+    document.addEventListener("pointermove", this.onThumbPointerMove);
+    document.addEventListener("pointerup", this.onThumbPointerUp);
+  };
+
+  onThumbPointerMove = (event) => {
+    const sliderProgress = this.element.querySelector(".range-slider__progress");
+    const leftSlider = this.element.querySelector(".range-slider__thumb-left");
+    const rightSlider = this.element.querySelector(".range-slider__thumb-right");
+    const fromElementValue = this.element.querySelector('[data-element="from"]');
+    const toElementValue = this.element.querySelector('[data-element="to"]');
+
+    if (this.activeThumb === "thumbLeft") {
+      this.from = this.processPointerMove(event);
+      const value = this.toPercent("left-thubm");
+      leftSlider.style.left = value + "%";
+      sliderProgress.style.left = value + "%";
+      fromElementValue.textContent = this.formatValue(this.from);
+    }
+
+    if (this.activeThumb === "thumbRight") {
+      this.to = this.processPointerMove(event);
+      const value = this.toPercent("right-thubm");
+      toElementValue.textContent = this.formatValue(this.to);
+      rightSlider.style.right = value + "%";
+      sliderProgress.style.right = value + "%";
+    }
+
+  };
+
+  onThumbPointerUp = (event) => {
+    this.dispatchCustomEvent();
+    document.removeEventListener("pointermove", this.onThumbPointerMove);
+    document.removeEventListener("pointerup", this.onThumbPointerUp);
+  };
+
+  dispatchCustomEvent() {
+    const event = new CustomEvent("range-select", {
+      detail: { from: this.from, to: this.to },
+    });
+    this.element.dispatchEvent(event);
   }
 
-  //  onThumbLeftPointerDown = (event) => {
-  //    document.addEventListener("pointermove", this.onThumbLeftPointerMove);
-  //    document.addEventListener("pointerup", this.onThumbLeftPointerUp);
-  //  };
+  createElement(template) {
+    const element = document.createElement("div");
+    element.classList.add("range-slider");
+    element.innerHTML = template;
+    return element;
+  }
 
-     onThumbPointerDown = (event) => {
-       this.activeThumb = event.target.dataset.element;
-       document.addEventListener("pointermove", this.onThumbPointerMove);
-       document.addEventListener("pointerup", this.onThumbPointerUp);
-     };
+  createEventListeners() {
+    this.element.addEventListener("pointerdown", this.onThumbPointerDown);
+  }
 
-          onThumbPointerMove = (event) => {
-            const sliderProgress = this.element.querySelector(".range-slider__progress");
-            const leftSlider = this.element.querySelector('.range-slider__thumb-left');
-            const rightSlider = this.element.querySelector('.range-slider__thumb-right');
-            const fromElementValue = this.element.querySelector('[data-element="from"]');
-            const toElementValue = this.element.querySelector('[data-element="to"]');
-            if (this.activeThumb === 'thumbLeft') {
-              this.from = this.processPointerMove(event);
-              const value = this.toPercent('left-thubm');
-              leftSlider.style.left = value + '%';
-              sliderProgress.style.left = value + '%';
-              fromElementValue.textContent = this.formatValue(this.from);
-            } if (this.activeThumb === 'thumbRight') {
-              this.to = this.processPointerMove(event);
-              const value = this.toPercent('right-thubm');
-              toElementValue.textContent = this.formatValue(this.to);
-              rightSlider.style.right = value + '%';
-              sliderProgress.style.right = value + '%';
-            }
-          };
+  removeEventListeners() {
+    this.element.removeEventListener("pointerdown", this.onThumbPointerDown);
+  }
 
-          onThumbPointerUp = (event) => {
-            this.dispatchCustomEvent();
-            document.removeEventListener("pointermove", this.onThumbPointerMove);
-            document.removeEventListener("pointerup", this.onThumbPointerUp);
-          };
-
-          // onThumbLeftPointerMove = (event) => {
-          //   const sliderProgress = document.querySelector(".range-slider__progress");
-          //   const leftSlider = document.querySelector('.range-slider__thumb-left');
-          //   const fromElementValue = document.querySelector('[data-element="from"]');
-
-          //   this.from = this.processPointerMove(event);
-          //   const value = this.toPercent('left-thubm');
-          //   leftSlider.style.left = value + '%';
-          //   sliderProgress.style.left = value + '%';
-          //   fromElementValue.textContent = this.formatValue(this.from);
-          // };
-
-          // onThumbLeftPointerUp = (event) => {
-          //   this.dispatchCustomEvent();
-          //   document.removeEventListener("pointermove", this.onThumbLeftPointerMove);
-          //   document.removeEventListener("pointerup", this.onThumbLeftPointerUp);
-          // };
-
-          // onThumbRightPointerDown = (event) => {
-          //   document.addEventListener("pointermove", this.onThumbRightPointerMove);
-          //   document.addEventListener("pointerup", this.onThumbRightPointerUp);
-          // };
-
-          // onThumbRightPointerMove = (event) => {
-          //   const sliderProgress = document.querySelector(".range-slider__progress");
-          //   const rightSlider = document.querySelector('.range-slider__thumb-right');
-          //   const toElementValue = document.querySelector('[data-element="to"]');
-
-          //   this.to = this.processPointerMove(event);
-          //   const value = this.toPercent('right-thubm');
-          //   toElementValue.textContent = this.formatValue(this.to);
-          //   rightSlider.style.right = value + '%';
-          //   sliderProgress.style.right = value + '%';
-          // };
-
-          // onThumbRightPointerUp = (event) => {
-          //   this.dispatchCustomEvent();
-          //   document.removeEventListener("pointermove", this.onThumbRightPointerMove);
-          //   document.removeEventListener("pointerup", this.onThumbRightPointerUp);
-          // };
-
-          dispatchCustomEvent () {
-            const event = new CustomEvent('range-select', {detail: {from: this.from, to: this.to}});
-            this.element.dispatchEvent(event);
-          }
-
-          createElement(template) {
-            const element = document.createElement("div");
-            element.classList.add("range-slider");
-            element.innerHTML = template;
-            return element;
-          }
-
-          // createEventListeners() {
-          //   const element = this.element;
-          //   const leftThumb = element.querySelector(".range-slider__thumb-left");
-          //   const rightThumb = element.querySelector(".range-slider__thumb-right");
-          //   const sliderProgress = element.querySelector(".range-slider__progress");
-          //   leftThumb.addEventListener("pointerdown", this.onThumbLeftPointerDown);
-          //   rightThumb.addEventListener("pointerdown", this.onThumbRightPointerDown);
-          // }
-
-          createEventListeners() {
-            this.element.addEventListener("pointerdown", this.onThumbPointerDown);
-          }
-
-          // removeEventListeners() {
-          //   const element = this.element;
-          //   const leftThumb = element.querySelector(".range-slider__thumb-left");
-          //   const rightThumb = element.querySelector(".range-slider__thumb-right");
-          //   const sliderProgress = element.querySelector(".range-slider__progress");
-          //   leftThumb.removeEventListener("pointerdown", this.onThumbLeftPointerDown);
-          //   rightThumb.removeEventListener("pointerdown", this.onThumbRightPointerDown);
-          // }
-
-          removeEventListeners() {
-            this.element.removeEventListener("pointerdown", this.onThumbPointerDown);
-          }
-
-          remove() {
-            this.element.remove();
-          }
-          destroy() {
-            this.remove();
-            this.removeEventListeners();
-          }
+  remove() {
+    this.element.remove();
+  }
+  destroy() {
+    this.remove();
+    this.removeEventListeners();
+  }
 }
